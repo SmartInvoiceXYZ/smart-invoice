ba7703b8a7c8082f15b8008a0cb8d3f28e5124e1
dan13ram <dan13ram@gmail.com>
2020-10-24 23:48:17 +0530

    stylistic changes

--- packages/contracts/contracts/SmartInvoiceMono.sol
+++ packages/contracts/contracts/SmartInvoiceMono.sol
@@ -14,8 +14,8 @@ interface IWETH {
   function deposit() external payable;
 }
 
-// splittable digital deal escrows w/ embedded arbitration tailored for guild work
-contract SmartEscrowMono is Context, IArbitrable, ReentrancyGuard {
+// splittable digital deal invoices w/ embedded arbitration tailored for guild work
+contract SmartInvoiceMono is Context, IArbitrable, ReentrancyGuard {
   using SafeMath for uint256;
   using SafeERC20 for IERC20;
 
@@ -37,23 +37,22 @@ contract SmartEscrowMono is Context, IArbitrable, ReentrancyGuard {
 
   /** kovan wETH **/
   address public wETH = 0xd0A1E359811322d97991E03f863a0C30C2cF029C;
-  uint256 public escrowCount;
-  uint256 public constant MAX_DURATION = 63113904; // 2-year limit on escrow
-  mapping(uint256 => Escrow) public escrows;
+  uint256 public invoiceCount;
+  uint256 public constant MAX_DURATION = 63113904; // 2-year limit on invoice
+  mapping(uint256 => Invoice) public invoices;
   mapping(uint256 => uint256) public disputes;
 
   enum ADR {LEX_DAO, ARAGON_COURT}
   
-  struct Escrow {
+  struct Invoice {
     address client;
     address provider;
     ADR resolverType;
     address resolver;
     address token;
-    // bool confirmed;
     bool locked;
     uint256[] amounts; // milestones split into amounts
-    uint256 milestone; // current milestone - starts from 0 to amounts.length
+    uint256 milestone; // current milestone - starts from 0 to amounts.length-1
     uint256 total;
     uint256 balance;
     uint256 released;
@@ -92,7 +91,7 @@ contract SmartEscrowMono is Context, IArbitrable, ReentrancyGuard {
   event Rule(address indexed resolver, uint256 index, uint256 ruling);
 
   function register(
-    // register escrow for token deposit & client deal confirmation
+    // register invoice for token deposit & client deal confirmation
     address client,
     address provider,
     uint8 resolverType,
@@ -114,10 +113,10 @@ contract SmartEscrowMono is Context, IArbitrable, ReentrancyGuard {
       total = total.add(amounts[i]);
     }
 
-    escrowCount = escrowCount + 1;
-    uint256 index = escrowCount;
+    invoiceCount = invoiceCount + 1;
+    uint256 index = invoiceCount;
 
-    escrows[index] = Escrow(
+    invoices[index] = Invoice(
       client,
       provider,
       ADR(resolverType),
@@ -138,92 +137,89 @@ contract SmartEscrowMono is Context, IArbitrable, ReentrancyGuard {
   }
 
   function deposit(uint256 index, uint256 amount) external payable nonReentrant {
-    // client (or anybody) deposits amount in escrow
-    Escrow storage escrow = escrows[index];
+    // client (or anybody) deposits amount in invoice
+    Invoice storage invoice = invoices[index];
 
-    require(!escrow.locked, "locked");
+    require(!invoice.locked, "locked");
     require(amount > 0, "amount is zero");
-    require(amount <= escrow.total - escrow.balance - escrow.released, "amount too large");
+    require(amount <= invoice.total - invoice.balance - invoice.released, "amount too large");
 
-    if (escrow.token == wETH && msg.value > 0) {
+    if (invoice.token == wETH && msg.value > 0) {
       require(msg.value == amount, "!ETH");
       IWETH(wETH).deposit{value: msg.value}();
     } else {
-      IERC20(escrow.token).safeTransferFrom(msg.sender, address(this), amount);
+      IERC20(invoice.token).safeTransferFrom(msg.sender, address(this), amount);
     }
-    escrow.balance = escrow.balance.add(amount);
+    invoice.balance = invoice.balance.add(amount);
 
     emit Deposit(index, amount);
   }
 
   function release(uint256 index) external nonReentrant {
-    // client transfers escrow milestone amount to provider
-    Escrow storage escrow = escrows[index];
+    // client transfers invoice milestone amount to provider
+    Invoice storage invoice = invoices[index];
 
-    // require(escrow.confirmed, "!confirmed");
-    require(!escrow.locked, "locked");
-    require(escrow.total > escrow.released, "released");
-    require(_msgSender() == escrow.client, "!client");
+    require(!invoice.locked, "locked");
+    require(invoice.total > invoice.released, "released");
+    require(_msgSender() == invoice.client, "!client");
 
-    uint256 currentMilestone = escrow.milestone;
-    escrow.milestone = escrow.milestone.add(1);
+    uint256 currentMilestone = invoice.milestone;
+    invoice.milestone = invoice.milestone.add(1);
 
-    uint256 amount = escrow.amounts[currentMilestone];
-    require(escrow.balance >= amount, "insufficient balance");
+    uint256 amount = invoice.amounts[currentMilestone];
+    require(invoice.balance >= amount, "insufficient balance");
 
-    IERC20(escrow.token).safeTransfer(escrow.provider, amount);
-    escrow.released = escrow.released.add(amount);
-    escrow.balance = escrow.balance.sub(amount);
+    IERC20(invoice.token).safeTransfer(invoice.provider, amount);
+    invoice.released = invoice.released.add(amount);
+    invoice.balance = invoice.balance.sub(amount);
 
     emit Release(index, currentMilestone, amount);
   }
 
   function withdraw(uint256 index) external nonReentrant {
-    // withdraw escrow balance to client if termination time passes & no lock
-    Escrow storage escrow = escrows[index];
+    // withdraw invoice balance to client if termination time passes & no lock
+    Invoice storage invoice = invoices[index];
 
-    // require(escrow.confirmed, "!confirmed");
-    require(!escrow.locked, "locked");
-    require(escrow.balance > 0, "balance is zero");
-    require(block.timestamp > escrow.terminationTime, "!terminated");
+    require(!invoice.locked, "locked");
+    require(invoice.balance > 0, "balance is zero");
+    require(block.timestamp > invoice.terminationTime, "!terminated");
 
-    IERC20(escrow.token).safeTransfer(escrow.client, escrow.balance);
+    IERC20(invoice.token).safeTransfer(invoice.client, invoice.balance);
 
-    emit Withdraw(index, escrow.balance);
+    emit Withdraw(index, invoice.balance);
   }
 
   function lock(uint256 index, bytes32 details) external nonReentrant {
-    // client or main (0) provider can lock balance for resolution during escrow period / update request details
-    Escrow storage escrow = escrows[index];
+    // client or main (0) provider can lock balance for resolution during invoice period / update request details
+    Invoice storage invoice = invoices[index];
 
-    // require(escrow.confirmed, "!confirmed");
-    require(!escrow.locked, "locked");
-    require(escrow.balance > 0, "balance is zero");
-    require(block.timestamp < escrow.terminationTime, "terminated");
+    require(!invoice.locked, "locked");
+    require(invoice.balance > 0, "balance is zero");
+    require(block.timestamp < invoice.terminationTime, "terminated");
     require(
-      _msgSender() == escrow.client || _msgSender() == escrow.provider,
+      _msgSender() == invoice.client || _msgSender() == invoice.provider,
       "!party"
     );
 
-    if (escrow.resolverType == ADR.ARAGON_COURT) {
-      IArbitrator arbitrator = IArbitrator(escrow.resolver);
-      payDisputeFees(escrow.resolver, escrow);
+    if (invoice.resolverType == ADR.ARAGON_COURT) {
+      IArbitrator arbitrator = IArbitrator(invoice.resolver);
+      _payDisputeFees(invoice.resolver, invoice);
       uint256 disputeId = arbitrator.createDispute(
         DISPUTES_POSSIBLE_OUTCOMES,
         abi.encodePacked(details)
       );
       disputes[disputeId] = index;
     }
-    escrow.locked = true;
+    invoice.locked = true;
 
     emit Lock(_msgSender(), index, details);
   }
 
-  function payDisputeFees(address _adr, Escrow storage escrow) internal {
+  function _payDisputeFees(address _adr, Invoice storage invoice) internal {
     IArbitrator arbitrator = IArbitrator(_adr);
     (, IERC20 feeToken, uint256 feeAmount) = arbitrator.getDisputeFees();
-    if (address(feeToken) == escrow.token) {
-      uint256 balance = escrow.balance;
+    if (address(feeToken) == invoice.token) {
+      uint256 balance = invoice.balance;
       // // sender can pay extra dispute fees
       // if (feeAmount > balance) {
       //   feeToken.safeTransferFrom(msg.sender, address(this), feeAmount - balance);
@@ -242,25 +238,25 @@ contract SmartEscrowMono is Context, IArbitrable, ReentrancyGuard {
     bytes32 details
   ) external nonReentrant {
     // called by lex dao
-    Escrow storage escrow = escrows[index];
-    require(escrow.resolverType == ADR.LEX_DAO, "!lex");
-    require(escrow.locked, "!locked");
-    require(escrow.balance > 0, "balance is zero");
-    require(_msgSender() == escrow.resolver, "!resolver");
+    Invoice storage invoice = invoices[index];
+    require(invoice.resolverType == ADR.LEX_DAO, "!lex");
+    require(invoice.locked, "!locked");
+    require(invoice.balance > 0, "balance is zero");
+    require(_msgSender() == invoice.resolver, "!resolver");
 
-    uint256 balance = escrow.balance;
+    uint256 balance = invoice.balance;
     uint256 resolutionFee = balance.div(20); // calculates dispute resolution fee (5% of balance)
 
     require(
       clientAward.add(providerAward) == balance.sub(resolutionFee),
       "resolution != balance"
     );
-    IERC20(escrow.token).safeTransfer(escrow.provider, providerAward);
-    IERC20(escrow.token).safeTransfer(escrow.client, clientAward);
-    IERC20(escrow.token).safeTransfer(escrow.resolver, resolutionFee);
+    IERC20(invoice.token).safeTransfer(invoice.provider, providerAward);
+    IERC20(invoice.token).safeTransfer(invoice.client, clientAward);
+    IERC20(invoice.token).safeTransfer(invoice.resolver, resolutionFee);
 
-    escrow.released = escrow.released.add(balance);
-    escrow.locked = false;
+    invoice.released = invoice.released.add(balance);
+    invoice.locked = false;
 
     emit Resolve(
       _msgSender(),
@@ -280,13 +276,13 @@ contract SmartEscrowMono is Context, IArbitrable, ReentrancyGuard {
     // called by aragon court
     require(_ruling <= rulings.length, "invalid ruling");
     uint256 index = disputes[_disputeId];
-    Escrow storage escrow = escrows[index];
-    require(escrow.resolverType == ADR.ARAGON_COURT, "!aragon");
-    require(escrow.locked, "!locked");
-    require(escrow.balance > 0, "balance is zero");
-    require(_msgSender() == escrow.resolver, "!resolver");
+    Invoice storage invoice = invoices[index];
+    require(invoice.resolverType == ADR.ARAGON_COURT, "!aragon");
+    require(invoice.locked, "!locked");
+    require(invoice.balance > 0, "balance is zero");
+    require(_msgSender() == invoice.resolver, "!resolver");
 
-    uint256 balance = escrow.balance;
+    uint256 balance = invoice.balance;
     uint8[] storage ruling = rulings[_ruling];
     uint8 clientShare = ruling[0];
     uint8 providerShare = ruling[1];
@@ -295,14 +291,14 @@ contract SmartEscrowMono is Context, IArbitrable, ReentrancyGuard {
     uint256 clientAward = balance.sub(providerAward);
 
     if (providerAward > 0) {
-      IERC20(escrow.token).safeTransfer(escrow.provider, providerAward);
+      IERC20(invoice.token).safeTransfer(invoice.provider, providerAward);
     }
     if (clientAward > 0) {
-      IERC20(escrow.token).safeTransfer(escrow.client, clientAward);
+      IERC20(invoice.token).safeTransfer(invoice.client, clientAward);
     }
 
-    escrow.released = escrow.released.add(balance);
-    escrow.locked = false;
+    invoice.released = invoice.released.add(balance);
+    invoice.locked = false;
 
     emit Rule(_msgSender(), index, _ruling);
   }
