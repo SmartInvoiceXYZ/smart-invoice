dc158c82b3ea562d78c17950f977efd0b1382065
Manolingam N <manolingam1996@gmail.com>
2020-11-03 21:56:45 +0530

    lint error fix

--- packages/dapp/src/pages/CreateInvoice.jsx
+++ packages/dapp/src/pages/CreateInvoice.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext, useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { withRouter } from 'react-router-dom';
 
 import { AppContext } from '../context/AppContext';
@@ -9,39 +9,52 @@ import PaymentDetailsForm from '../components/PaymentDetailsForm';
 import PaymentChunksForm from '../components/PaymentChunksForm';
 import FormConfirmation from '../components/FormConfirmation';
 
-import '../sass/createInvoiceStyles.scss'
+import '../sass/createInvoiceStyles.scss';
 
 const { steps } = require('../utils/Constants');
 
-const CreateInvoice = (props) => {
-    const context = useContext(AppContext);
-    const [currentStep, setStep] = useState(1)
+const CreateInvoice = props => {
+  const context = useContext(AppContext);
+  const [currentStep, setStep] = useState(1);
 
-    // useEffect(() => {
-    //     if (context.address === '') return props.history.push('/')
-    //     // eslint-disable-next-line react-hooks/exhaustive-deps
-    // }, [])
+  // useEffect(() => {
+  //     if (context.address === '') return props.history.push('/')
+  //     // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [])
 
-    const stepHandler = () => {
-        if (currentStep === 4) return props.history.push('/success')
-        setStep((prevState) => prevState + 1);
-    }
+  const stepHandler = () => {
+    if (currentStep === 4) return props.history.push('/success');
+    setStep(prevState => prevState + 1);
+  };
 
-    return (
-        <div className='create-invoice'>
-            <StepInfo stepNum={currentStep} stepTitle={steps[currentStep].step_title} stepDetails={steps[currentStep].step_details} />
-            <div>
-                {currentStep === 1 && <ProjectDetailsForm context={context} />}
-                {currentStep === 2 && <PaymentDetailsForm context={context} />}
-                {currentStep === 3 && <PaymentChunksForm context={context} />}
-                {currentStep === 4 && <FormConfirmation context={context} />}
-                <div className='form-action-buttons'>
-                    {currentStep !== 1 && <button id='back-button' onClick={() => setStep((prevState) => prevState - 1)}>BACK</button>}
-                    <button id='next-button' onClick={stepHandler}>next: {steps[currentStep].next}</button>
-                </div>
-            </div>
+  return (
+    <div className="create-invoice">
+      <StepInfo
+        stepNum={currentStep}
+        stepTitle={steps[currentStep].step_title}
+        stepDetails={steps[currentStep].step_details}
+      />
+      <div>
+        {currentStep === 1 && <ProjectDetailsForm context={context} />}
+        {currentStep === 2 && <PaymentDetailsForm context={context} />}
+        {currentStep === 3 && <PaymentChunksForm context={context} />}
+        {currentStep === 4 && <FormConfirmation context={context} />}
+        <div className="form-action-buttons">
+          {currentStep !== 1 && (
+            <button
+              id="back-button"
+              onClick={() => setStep(prevState => prevState - 1)}
+            >
+              BACK
+            </button>
+          )}
+          <button id="next-button" onClick={stepHandler}>
+            next: {steps[currentStep].next}
+          </button>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default withRouter(CreateInvoice);
