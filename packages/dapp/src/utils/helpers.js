17f5550058ea6d8efd8eb9ebcc9172eff126637c
dan13ram <dan13ram@gmail.com>
2021-01-19 09:35:00 +0530

    firebase deployment

--- packages/dapp/src/utils/helpers.js
+++ packages/dapp/src/utils/helpers.js
@@ -52,3 +52,7 @@ export const getTxLink = hash => `https://rinkeby.etherscan.io/tx/${hash}`;
 
 export const getAddressLink = hash =>
   `https://rinkeby.etherscan.io/address/${hash}`;
+
+export const getInvoiceStatus = _invoice => {
+  return { funded: true, label: 'FUNDED' };
+};
