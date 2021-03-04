149693b46e36522030bcde24ae7438c970eb1d5c
dan13ram <dan13ram@gmail.com>
2021-03-04 22:19:09 +0530

    fixed url regex

--- packages/dapp/src/utils/helpers.js
+++ packages/dapp/src/utils/helpers.js
@@ -117,12 +117,11 @@ export const copyToClipboard = value => {
 
 export const isValidHttpsURL = str => {
   const pattern = new RegExp(
-    '^(https:\\/\\/)?' + // protocol
+    '^(?:https:\\/\\/)' + // protocol
       '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
       '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-      '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-      '(\\#[-a-z\\d_]*)?$',
+      '(?::\\d{2,5})?' + // port
+      '(?:[/?#][^\\s"]*)?', // path
     'i',
   ); // fragment locator
   return !!pattern.test(str);
