8555931e146d52051597bfdb5cf80c32327c2bae
Peter Sparacino <72105234+psparacino@users.noreply.github.com>
2022-06-14 11:28:19 -0400

    changed verify visibility

--- packages/contracts/contracts/SmartInvoice.sol
+++ packages/contracts/contracts/SmartInvoice.sol
@@ -144,7 +144,7 @@ contract SmartInvoice is
     }
 
     // Client verifies address before deposits
-    function verify() public {
+    function verify() external {
         require(msg.sender == client, "!client");
         emit Verified(client, address(this));
     }
