5ba88961bbeb664272aa94a56b012feb8c7cbd20
George <gbh218@gmail.com>
2022-09-03 22:37:29 -0400

    Add mobile ui responsiveness

--- packages/dapp/src/pages/Home.jsx
+++ packages/dapp/src/pages/Home.jsx
@@ -6,18 +6,30 @@ import {
   useBreakpointValue,
   useDisclosure,
 } from '@chakra-ui/react';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
-import { WhatIsThisModal } from '../components/WhatIsThisModal';
 import { useWeb3 } from '../context/Web3Context';
-import { Container } from '../shared/Container';
 import { logError } from '../utils/helpers';
 
 export const Home = () => {
   const { connectAccount, account } = useWeb3();
 
   const history = useHistory();
+  const [isMobile, onMobile] = useState(false);
+  useEffect(() => {
+    if (window) {
+      toggleMobileMode();
+      window.addEventListener('resize', toggleMobileMode);
+    }
+  });
+  const toggleMobileMode = () => {
+    if (window.innerWidth < 600) {
+      onMobile(true);
+    } else {
+      onMobile(false);
+    }
+  };
 
   const createInvoice = async () => {
     if (account) {
@@ -46,10 +58,6 @@ export const Home = () => {
   };
 
   const buttonSize = useBreakpointValue({ base: 'sm', sm: 'md', md: 'lg' });
-  const fontSize = useBreakpointValue({ base: 'lg', sm: 'xl', md: '2xl' });
-  const smallScreen = useBreakpointValue({ base: true, sm: false });
-  const betaWarningSmallScreen = useBreakpointValue({ base: true, lg: false });
-  const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <Flex direction="column" align="center" justify="center" gap={6}>
@@ -64,7 +72,15 @@ export const Home = () => {
       <Text fontStyle="italic" color="grey">
         How do you want to get started?
       </Text>
-      <Flex gap={10} width="100%" align="stretch" justify="center">
+      <Flex
+        direction={isMobile && 'column'}
+        columnGap={10}
+        rowGap={4}
+        width="100%"
+        align="stretch"
+        justify="center"
+        paddingX={10}
+      >
         <Button
           _hover={{ backgroundColor: 'rgba(61, 136, 248, 0.7)' }}
           _active={{ backgroundColor: 'rgba(61, 136, 248, 0.7)' }}
@@ -73,6 +89,7 @@ export const Home = () => {
           onClick={createInvoice}
           size={buttonSize}
           minW="250px"
+          paddingY={6}
         >
           Create Invoice
         </Button>
@@ -84,82 +101,11 @@ export const Home = () => {
           onClick={viewInvoices}
           size={buttonSize}
           minW="250px"
+          paddingY={6}
         >
           View Existing Invoices
         </Button>
       </Flex>
-      {/* <Flex
-        direction="row"
-        align="stretch"
-        m={{ base: '1rem', md: '2rem', lg: '4rem' }}
-        mr={{ base: '1rem', md: '2rem', lg: '2rem' }}
-        // w={{ base: '22rem', sm: '28rem', lg: '32rem' }}
-        // maxW="calc(100% - 2rem)"
-      >
-        <Button
-          _hover={{ backgroundColor: 'rgba(61, 136, 248, 0.7)' }}
-          _active={{ backgroundColor: 'rgba(61, 136, 248, 0.7)' }}
-          color="white"
-          backgroundColor="blue.1"
-          onClick={createInvoice}
-          // size={buttonSize}
-          // width='fit-content'
-          fontFamily="mono"
-          fontWeight="normal"
-        >
-          CREATE NEW INVOICE
-        </Button>
-        <Text
-          fontWeight="bold"
-          my="0.5rem"
-          w="100%"
-          textAlign="center"
-          fontSize={{ base: 'md', md: 'xl' }}
-        >
-          or
-        </Text>
-        <Button
-          _hover={{ backgroundColor: 'rgba(61, 136, 248, 0.7)' }}
-          _active={{ backgroundColor: 'rgba(61, 136, 248, 0.7)' }}
-          color="white"
-          backgroundColor="blue.1"
-          onClick={viewInvoices}
-          size={buttonSize}
-          fontFamily="mono"
-          fontWeight="normal"
-        >
-          VIEW EXISTING INVOICES
-        </Button>
-        <Button
-          mt="2rem"
-          variant="link"
-          color="black"
-          textDecor="underline"
-          size={buttonSize}
-          fontSize={fontSize}
-          onClick={onOpen}
-          fontFamily="mono"
-          fontWeight="normal"
-          mx="auto"
-          w="auto"
-        >
-          What is this?
-        </Button>
-        <WhatIsThisModal isOpen={isOpen} onClose={onClose} />
-      </Flex>
-      <Flex
-        {...(betaWarningSmallScreen
-          ? {}
-          : {
-              width: '100%',
-              justify: 'flex-end',
-              align: 'flex-end',
-              justifySelf: 'flex-end',
-              alignSelf: 'flex-end',
-            })}
-        py="3rem"
-        m="1rem"
-      ></Flex> */}
     </Flex>
   );
 };
--- packages/dapp/src/shared/Footer.jsx
+++ packages/dapp/src/shared/Footer.jsx
@@ -1,25 +1,34 @@
 import { Box, Flex, Link as ChakraLink, Image } from '@chakra-ui/react';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import logo from '../assets/smart-invoice/white.svg';
 
 export function Footer() {
+  const [isMobile, onMobile] = useState(false);
+  useEffect(() => {
+    if (window) {
+      toggleMobileMode();
+      window.addEventListener('resize', toggleMobileMode);
+    }
+  });
+  const toggleMobileMode = () => {
+    if (window.innerWidth < 750) {
+      onMobile(true);
+    } else {
+      onMobile(false);
+    }
+  };
+
   return (
-    <Box
-      background="#334D6E"
-      // position="absolute"
-      // bottom="0"
-      // right="0"
-      width="100%"
-      alignSelf="end"
-      zIndex={5}
-    >
+    <Box background="#334D6E" width="100%" alignSelf="end" zIndex={5}>
       <Flex
+        direction={isMobile ? 'column-reverse' : 'row'}
         justify="space-between"
         align="center"
         paddingX={20}
         paddingY={4}
         textColor="white"
+        rowGap={4}
       >
         <ChakraLink href="/invoices">
           <Image src={logo} alt="Smart Invoice" width={160} height={25.34} />
