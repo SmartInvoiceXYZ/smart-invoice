d65325c20fb4eefed33e20d220280faeef71a3a0
Peter Sparacino <72105234+psparacino@users.noreply.github.com>
2022-09-19 13:03:25 -0400

    updated mainnet endpoint

--- packages/dapp/src/config.js
+++ packages/dapp/src/config.js
@@ -6,7 +6,7 @@ export const CONFIG = {
   BOX_ENDPOINT: 'https://ipfs.3box.io',
   NETWORK_CONFIG: {
     1: {
-      SUBGRAPH: 'mainnet-smart-invoice/v0.0.1',
+      SUBGRAPH: 'psparacino/mainnet-smart-invoices',
       WRAPPED_NATIVE_TOKEN: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'.toLowerCase(),
       INVOICE_FACTORY: '0xe73251284989c4496985f16c718369bbc30ccf63'.toLowerCase(),
       RESOLVERS: {
