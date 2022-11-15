8c2337552d05abf79d582e5c46da0acfd52225b8
Peter Sparacino <72105234+psparacino@users.noreply.github.com>
2022-11-14 20:13:03 -0500

    record implementations in deployments

--- packages/contracts/scripts/add-escrow.js
+++ packages/contracts/scripts/add-escrow.js
@@ -7,15 +7,6 @@ const xdai = require("../deployments/xdai.json");
 const abi =
   require("../build/contracts/SmartInvoiceFactory.sol/SmartInvoiceFactory.json").abi;
 
-const wrappedTokenAddress = {
-  1: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
-  4: "0xc778417e063141139fce010982780140aa0cd5ab",
-  5: "0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6",
-  42: "0xd0a1e359811322d97991e03f863a0c30c2cf029c",
-  77: "0xc655c6D80ac92d75fBF4F40e95280aEb855B1E87",
-  100: "0xe91d153e0b41518a2ce8dd3d7944fa863463a97d",
-};
-
 const networkName = {
   1: "mainnet",
   4: "rinkeby",
@@ -36,9 +27,6 @@ const networkCurrency = {
   31337: "localhost",
 };
 
-// const hre = require('hardhat');
-// const factory = await hre.ethers.getContractAt("SmartInvoiceFactory", "0x546adED0B0179d550e87cf909939a1207Fd26fB7");
-
 const BLOCKSCOUT_CHAIN_IDS = [77, 100];
 
 const escrowType = ethers.utils.formatBytes32String("escrow");
@@ -53,7 +41,7 @@ async function main() {
     abi,
     deployer,
   );
-  console.log(escrowType);
+
   console.log(
     "Adding Implementation on",
     networkName[chainId],
@@ -67,23 +55,22 @@ async function main() {
     networkCurrency[chainId],
   );
 
-  const SmartInvoice = await ethers.getContractFactory("SmartInvoiceEscrow");
-  const smartInvoice = await SmartInvoice.deploy();
-  await smartInvoice.deployed();
-  console.log("Implementation Address:", smartInvoice.address);
+  const SmartInvoiceEscrow = await ethers.getContractFactory(
+    "SmartInvoiceEscrow",
+  );
+  const smartInvoiceEscrow = await SmartInvoiceEscrow.deploy();
+  await smartInvoiceEscrow.deployed();
+  console.log("Deployed Implementation Address:", smartInvoiceEscrow.address);
 
-  await smartInvoice.initLock();
+  await smartInvoiceEscrow.initLock();
 
   const implementationReceipt = await factory
     .connect(deployer)
-    .addImplementation(escrowType, smartInvoice.address);
+    .addImplementation(escrowType, smartInvoiceEscrow.address);
 
   await implementationReceipt.wait();
 
   const version = await factory.currentVersions(escrowType);
-
-  console.log("Implementation Version", version);
-
   const implementationAdded = await factory.implementations(
     escrowType,
     version,
@@ -109,6 +96,19 @@ async function main() {
     console.log(verifyResult);
     console.log("Verified Implementation");
   }
+
+  const data = fs.readFileSync(`deployments/${network.name}.json`, {
+    encoding: "utf8",
+  });
+
+  const deployment = JSON.parse(data);
+
+  deployment.implementations.push(smartInvoiceEscrow.address);
+
+  fs.writeFileSync(
+    `deployments/${network.name}.json`,
+    JSON.stringify(deployment, undefined, 2),
+  );
 }
 
 main()
