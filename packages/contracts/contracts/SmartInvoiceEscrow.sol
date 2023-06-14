b08f51b1283c513597863066e101ecee60f4b4f2
geovgy <gbh218@gmail.com>
2023-06-13 20:59:33 -0400

    Inherit and override SmartInvoiceEscrow

--- packages/contracts/contracts/SmartInvoiceEscrow.sol
+++ packages/contracts/contracts/SmartInvoiceEscrow.sol
@@ -104,7 +104,7 @@ contract SmartInvoiceEscrow is
         address _recipient,
         uint256[] calldata _amounts,
         bytes calldata _data
-    ) external override initializer {
+    ) external virtual override initializer {
         require(_recipient != address(0), "invalid provider");
 
         _handleData(_data);
@@ -122,7 +122,7 @@ contract SmartInvoiceEscrow is
      * @dev Handles the provided data, decodes it, and initializes necessary contract state variables.
      * @param _data The data to be handled and decoded
      */
-    function _handleData(bytes calldata _data) internal {
+    function _handleData(bytes calldata _data) internal virtual {
         (
             address _client,
             uint8 _resolverType,
@@ -256,7 +256,7 @@ contract SmartInvoiceEscrow is
     /**
      * @dev Internal function to release funds from the contract to the provider.
      */
-    function _release() internal {
+    function _release() internal virtual {
         // client transfers locker milestone funds to provider
 
         require(!locked, "locked");
@@ -289,7 +289,7 @@ contract SmartInvoiceEscrow is
      * @dev External function to release funds from the contract to the provider.
      * Uses the internal `_release` function to perform the actual release.
      */
-    function release() external override nonReentrant {
+    function release() external virtual override nonReentrant {
         return _release();
     }
 
@@ -297,7 +297,12 @@ contract SmartInvoiceEscrow is
      * Uses the internal `_release` function to perform the actual release.
      * @param _milestone The milestone to release funds to
      */
-    function release(uint256 _milestone) external override nonReentrant {
+    function release(uint256 _milestone)
+        external
+        virtual
+        override
+        nonReentrant
+    {
         // client transfers locker funds upto certain milestone to provider
         require(!locked, "locked");
         require(_msgSender() == client, "!client");
@@ -326,7 +331,12 @@ contract SmartInvoiceEscrow is
      * Uses the internal `_release` function to perform the actual release.
      * @param _token The milestones to release funds to
      */
-    function releaseTokens(address _token) external override nonReentrant {
+    function releaseTokens(address _token)
+        external
+        virtual
+        override
+        nonReentrant
+    {
         if (_token == token) {
             _release();
         } else {
@@ -408,7 +418,7 @@ contract SmartInvoiceEscrow is
         uint256 _clientAward,
         uint256 _providerAward,
         bytes32 _details
-    ) external override nonReentrant {
+    ) external virtual override nonReentrant {
         // called by individual
         require(resolverType == ADR.INDIVIDUAL, "!individual resolver");
         require(locked, "!locked");
@@ -452,6 +462,7 @@ contract SmartInvoiceEscrow is
      */
     function rule(uint256 _disputeId, uint256 _ruling)
         external
+        virtual
         override
         nonReentrant
     {
--- packages/contracts/contracts/SmartInvoiceSplitEscrow.sol
+++ packages/contracts/contracts/SmartInvoiceSplitEscrow.sol
@@ -6,96 +6,24 @@ pragma experimental ABIEncoderV2;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/utils/Context.sol";
+// import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+// import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
-import "./interfaces/ISmartInvoiceEscrow.sol";
-import "./interfaces/ISmartInvoiceFactory.sol";
-import "./interfaces/IArbitrable.sol";
-import "./interfaces/IArbitrator.sol";
-import "./interfaces/IWRAPPED.sol";
+// import "./interfaces/ISmartInvoiceEscrow.sol";
+// import "./interfaces/ISmartInvoiceFactory.sol";
+// import "./interfaces/IArbitrable.sol";
+// import "./interfaces/IArbitrator.sol";
+// import "./interfaces/IWRAPPED.sol";
+import "./SmartInvoiceEscrow.sol";
 
 // splittable digital deal lockers w/ embedded arbitration tailored for guild work
-contract SmartInvoiceSplitEscrow is
-    ISmartInvoiceEscrow,
-    IArbitrable,
-    Initializable,
-    Context,
-    ReentrancyGuard
-{
+contract SmartInvoiceSplitEscrow is SmartInvoiceEscrow {
     using SafeERC20 for IERC20;
 
-    uint256 public constant NUM_RULING_OPTIONS = 5; // excludes options 0, 1 and 2
-    // Note that Aragon Court treats the possible outcomes as arbitrary numbers, leaving the Arbitrable (us) to define how to understand them.
-    // Some outcomes [0, 1, and 2] are reserved by Aragon Court: "missing", "leaked", and "refused", respectively.
-    // Note that Aragon Court emits the LOWEST outcome in the event of a tie.
-
-    // solhint-disable-next-line var-name-mixedcase
-    uint8[2][6] public RULINGS = [
-        [1, 1], // 0 = refused to arbitrate
-        [1, 0], // 1 = 100% to client
-        [3, 1], // 2 = 75% to client
-        [1, 1], // 3 = 50% to client
-        [1, 3], // 4 = 25% to client
-        [0, 1] // 5 = 0% to client
-    ];
-
-    uint256 public constant MAX_TERMINATION_TIME = 63113904; // 2-year limit on locker
-
-    address public wrappedNativeToken;
-
-    enum ADR {
-        INDIVIDUAL,
-        ARBITRATOR
-    }
-
-    address public client;
-    address public provider;
     address public dao;
     uint256 public daoFee;
-    ADR public resolverType;
-    address public resolver;
-    address public token;
-    uint256 public terminationTime;
-    uint256 public resolutionRate;
-    bytes32 public details;
     mapping(uint256 => bool) public milestoneReleased;
     uint256 internal _remaining;
-
-    uint256[] public amounts; // milestones split into amounts
-    uint256 public total = 0;
-    bool public locked;
-    uint256 public milestone = 0; // current milestone - starts from 0 to amounts.length
-    uint256 public released = 0;
-    uint256 public disputeId;
-
-    event MilestonesAdded(
-        address indexed sender,
-        address indexed invoice,
-        uint256[] milestones
-    );
-    event DetailsUpdated(address indexed sender, bytes32 details);
-    event Deposit(address indexed sender, uint256 amount);
-    event Release(uint256 milestone, uint256 amount);
-    event Withdraw(uint256 balance);
-    event Lock(address indexed sender, bytes32 details);
-    event Resolve(
-        address indexed resolver,
-        uint256 clientAward,
-        uint256 providerAward,
-        uint256 resolutionFee,
-        bytes32 details
-    );
-    event Rule(
-        address indexed resolver,
-        uint256 clientAward,
-        uint256 providerAward,
-        uint256 ruling
-    );
-    event Verified(address indexed client, address indexed invoice);
-
-    // solhint-disable-next-line no-empty-blocks
-    function initLock() external initializer {}
 
     /**
      * @dev Initializes the contract with the provided recipient, amounts, and data.
@@ -127,7 +55,7 @@ contract SmartInvoiceSplitEscrow is
      * @dev Handles the provided data, decodes it, and initializes necessary contract state variables.
      * @param _data The data to be handled and decoded
      */
-    function _handleData(bytes calldata _data) internal {
+    function _handleData(bytes calldata _data) internal override {
         (
             address _client,
             uint8 _resolverType,
@@ -194,78 +122,6 @@ contract SmartInvoiceSplitEscrow is
     }
 
     /**
-     * @dev Verifies the client and contract are paired
-     */
-    function verify() external {
-        require(msg.sender == client, "!client");
-        emit Verified(client, address(this));
-    }
-
-    /**
-     * @dev Adds milestones without extra details.
-     * @param _milestones The array of new milestones to be added
-     */
-    function addMilestones(uint256[] calldata _milestones) external {
-        _addMilestones(_milestones, bytes32(0));
-    }
-
-    /**
-     * @dev Adds milestones with extra details.
-     * @param _milestones The array of new milestones to be added
-     * @param _details Additional details for the milestones
-     */
-    function addMilestones(uint256[] calldata _milestones, bytes32 _details)
-        external
-    {
-        _addMilestones(_milestones, _details);
-    }
-
-    /**
-     * @dev Internal function to add milestones and update the contract state.
-     * @param _milestones The array of new milestones to be added
-     * @param _details Additional details for the milestones
-     */
-    function _addMilestones(uint256[] calldata _milestones, bytes32 _details)
-        internal
-    {
-        require(!locked, "locked");
-        require(block.timestamp < terminationTime, "terminated");
-        require(_msgSender() == client || _msgSender() == provider, "!party");
-        require(_milestones.length > 0, "no milestones are being added");
-        require(_milestones.length <= 10, "only 10 new milestones at a time");
-
-        uint256 newLength = amounts.length + _milestones.length;
-        uint256[] memory baseArray = new uint256[](newLength);
-        uint256 newTotal = total;
-
-        for (uint256 i = 0; i < amounts.length; i++) {
-            baseArray[i] = amounts[i];
-        }
-        for (uint256 i = amounts.length; i < newLength; i++) {
-            baseArray[i] = _milestones[i - amounts.length];
-            newTotal += _milestones[i - amounts.length];
-        }
-
-        total = newTotal;
-        amounts = baseArray;
-
-        if (_details != bytes32(0)) {
-            details = _details;
-            emit DetailsUpdated(msg.sender, _details);
-        }
-
-        emit MilestonesAdded(msg.sender, address(this), _milestones);
-    }
-
-    /**
-     * @dev Returns the amounts associated with the milestones.
-     * @return An array of amounts for each milestone
-     */
-    function getAmounts() public view returns (uint256[] memory) {
-        return amounts;
-    }
-
-    /**
      * @dev Internal function to release funds from the contract to the provider.
      */
     function _release(uint256 _milestone) internal {
@@ -310,7 +166,7 @@ contract SmartInvoiceSplitEscrow is
     /**
      * @dev Internal function to release funds from the contract to the provider.
      */
-    function _release() internal {
+    function _release() internal override {
         // client transfers locker milestone funds to provider and dao
         uint256 currentMilestone = amounts.length - 1;
         for (uint256 i = 0; i < amounts.length; i++) {
@@ -405,68 +261,6 @@ contract SmartInvoiceSplitEscrow is
     }
 
     /**
-     * @dev Internal function to withdraw funds from the contract to the client.
-     */
-    function _withdraw() internal {
-        require(!locked, "locked");
-        require(block.timestamp > terminationTime, "!terminated");
-        uint256 balance = IERC20(token).balanceOf(address(this));
-        require(balance > 0, "balance is 0");
-
-        IERC20(token).safeTransfer(client, balance);
-        milestone = amounts.length;
-
-        emit Withdraw(balance);
-    }
-
-    /**
-     * @dev External function to withdraw funds from the contract to the client.
-     * Uses the internal `_withdraw` function to perform the actual withdrawal.
-     */
-    function withdraw() external override nonReentrant {
-        return _withdraw();
-    }
-
-    /**
-     * @dev External function to withdraw funds from the contract to the client.
-     * Uses the internal `_withdraw` function to perform the actual withdrawal.
-     * @param _token The token to withdraw
-     */
-    function withdrawTokens(address _token) external override nonReentrant {
-        if (_token == token) {
-            _withdraw();
-        } else {
-            require(block.timestamp > terminationTime, "!terminated");
-            uint256 balance = IERC20(_token).balanceOf(address(this));
-            require(balance > 0, "balance is 0");
-
-            IERC20(_token).safeTransfer(client, balance);
-        }
-    }
-
-    /**
-     * @dev External function to lock the contract.
-     * @param _details Details of the dispute
-     */
-    function lock(bytes32 _details) external payable override nonReentrant {
-        require(!locked, "locked");
-        uint256 balance = IERC20(token).balanceOf(address(this));
-        require(balance > 0, "balance is 0");
-        require(block.timestamp < terminationTime, "terminated");
-        require(_msgSender() == client || _msgSender() == provider, "!party");
-
-        if (resolverType == ADR.ARBITRATOR) {
-            disputeId = IArbitrator(resolver).createDispute{value: msg.value}(
-                NUM_RULING_OPTIONS,
-                abi.encodePacked(details)
-            );
-        }
-        locked = true;
-
-        emit Lock(_msgSender(), _details);
-    }
-
-    /**
      * @dev External function to unlock the contract.
      * @param _clientAward The amount to award the client
      * @param _providerAward The amount to award the provider
@@ -555,33 +349,5 @@ contract SmartInvoiceSplitEscrow is
 
         emit Rule(resolver, clientAward, providerAward, _ruling);
         emit Ruling(resolver, _disputeId, _ruling);
-    }
-
-    /**
-     * @dev Internal function to get the ruling of the arbitrator.
-     * @param _ruling The ruling of the arbitrator
-     */
-    function _getRuling(uint256 _ruling)
-        internal
-        pure
-        returns (uint8[2] memory ruling)
-    {
-        uint8[2][6] memory rulings = [
-            [1, 1], // 0 = refused to arbitrate
-            [1, 0], // 1 = 100% to client
-            [3, 1], // 2 = 75% to client
-            [1, 1], // 3 = 50% to client
-            [1, 3], // 4 = 25% to client
-            [0, 1] // 5 = 0% to client
-        ];
-        ruling = rulings[_ruling];
-    }
-
-    // receive eth transfers
-    receive() external payable {
-        require(!locked, "locked");
-        require(token == wrappedNativeToken, "!wrappedNativeToken");
-        IWRAPPED(wrappedNativeToken).deposit{value: msg.value}();
-        emit Deposit(_msgSender(), msg.value);
     }
 }
