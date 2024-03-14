4810580824688587ea49c74d557d643cf5702b2f
scottrepreneur <me@stwoh.com>
2024-03-14 06:04:40 -0500

    fix build

--- packages/dapp/next.config.js
+++ packages/dapp/next.config.js
@@ -1,29 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: [
-    '@rainbow-me/rainbowkit',
-    '@smart-invoice/constants',
-    '@smart-invoice/forms',
-    '@smart-invoice/graphql',
-    '@smart-invoice/hooks',
-    '@smart-invoice/types',
-    '@smart-invoice/ui',
-    '@smart-invoice/utils',
-  ],
+  transpilePackages: ['@rainbow-me/rainbowkit'],
   experimental: {
-    // optimizePackageImports: [
-    //   // '@chakra-ui/react',
-    //   '@rainbow-me/rainbowkit',
-    //   '@smart-invoice/constants',
-    //   '@smart-invoice/forms',
-    //   '@smart-invoice/graphql',
-    //   '@smart-invoice/hooks',
-    //   '@smart-invoice/types',
-    //   '@smart-invoice/ui',
-    //   '@smart-invoice/utils',
-    //   'viem',
-    //   'wagmi',
-    // ],
+    optimizePackageImports: ['@rainbow-me/rainbowkit'],
   },
 };
 
