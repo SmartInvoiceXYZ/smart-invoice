9eac4c7365ac8814b8f789519af97bb374a09a8c
dan13ram <dan13ram@gmail.com>
2024-08-20 15:48:51 +0530

    updated tsconfig for forms & ui + next.config.js for dapp

--- packages/dapp/next.config.js
+++ packages/dapp/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['@rainbow-me/rainbowkit'],
+  transpilePackages: [
+    '@rainbow-me/rainbowkit',
+    '@smartinvoicexyz/constants',
+    '@smartinvoicexyz/forms',
+    '@smartinvoicexyz/graphql',
+    '@smartinvoicexyz/hooks',
+    '@smartinvoicexyz/types',
+    '@smartinvoicexyz/ui',
+    '@smartinvoicexyz/utils',
+  ],
   experimental: {
     optimizePackageImports: ['@rainbow-me/rainbowkit'],
   },
