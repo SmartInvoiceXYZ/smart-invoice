1a96db25317f7affbf4f4047a0bb9c9fd8531c22
dan13ram <10012209+dan13ram@users.noreply.github.com>
2024-09-15 12:49:20 +0530

    added default tokens for sepolia & holesky

--- packages/hooks/src/useFetchTokens.ts
+++ packages/hooks/src/useFetchTokens.ts
@@ -2,6 +2,41 @@ import { IToken } from '@smartinvoicexyz/types';
 import { logError } from '@smartinvoicexyz/utils';
 import { useQuery } from '@tanstack/react-query';
 
+const DEFAULT_TOKENS: IToken[] = [
+  {
+    chainId: 11155111,
+    address: '0xdbae4073478cb2cbaba7206485c63c7291f464de',
+    symbol: 'MOCK',
+    name: 'MockToken',
+    decimals: 18,
+    logoURI: 'ipfs://QmaNmAVohHYtCFn8H9Lk393KDyj1rhWLUamGggFXM2eGLT',
+  },
+  {
+    chainId: 17000,
+    address: '0x06f85c9a4a36e5690b32e73cfdbf280766efd20b',
+    symbol: 'MOCK',
+    name: 'MockToken',
+    decimals: 18,
+    logoURI: 'ipfs://QmaNmAVohHYtCFn8H9Lk393KDyj1rhWLUamGggFXM2eGLT',
+  },
+  {
+    chainId: 11155111,
+    address: '0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14',
+    symbol: 'WETH',
+    name: 'Wrapped Ether',
+    decimals: 18,
+    logoURI: 'ipfs://QmPt2DEXhRSvbbFQep44XXtcKxRscshsijJ47Y1xUQwBZ1',
+  },
+  {
+    chainId: 17000,
+    address: '0x94373a4919b3240d86ea41593d5eba789fef3848',
+    symbol: 'WETH',
+    name: 'Wrapped Ether',
+    decimals: 18,
+    logoURI: 'ipfs://QmPt2DEXhRSvbbFQep44XXtcKxRscshsijJ47Y1xUQwBZ1',
+  },
+];
+
 const fetchTokens = async () => {
   const IPFS_TOKENS = `https://t2crtokens.eth.limo/`;
 
@@ -23,5 +58,7 @@ export const useFetchTokens = () => {
     staleTime: Infinity, // 1000 * 60 * 60 * 24,
   });
 
-  return { data, error, isLoading };
+  const allTokens = data ? [...DEFAULT_TOKENS, ...data] : DEFAULT_TOKENS;
+
+  return { data: allTokens, error, isLoading };
 };
