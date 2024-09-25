4f424b28e4cd45c0bfd72129e4fb34aa32f04f4e
dan13ram <dan13ram@gmail.com>
2024-09-25 10:39:59 +0530

    added multiple rpcs

--- packages/utils/src/web3.ts
+++ packages/utils/src/web3.ts
@@ -10,7 +10,7 @@ import {
 } from '@rainbow-me/rainbowkit/wallets';
 import _ from 'lodash';
 import { Chain, http, Transport } from 'viem';
-import { Config } from 'wagmi';
+import { Config, fallback } from 'wagmi';
 import {
   arbitrum,
   base,
@@ -25,6 +25,7 @@ import {
 const APP_NAME = 'Smart Invoice';
 const PROJECT_ID = process.env.NEXT_PUBLIC_WALLETCONNECT_ID || '';
 const INFURA_ID = process.env.NEXT_PUBLIC_INFURA_ID || '';
+const ALCHEMY_ID = process.env.NEXT_PUBLIC_ALCHEMY_ID || '';
 
 const infuraNetworkName: { [key: number]: string } = {
   [mainnet.id]: 'mainnet',
@@ -34,6 +35,16 @@ const infuraNetworkName: { [key: number]: string } = {
   [sepolia.id]: 'sepolia',
   [base.id]: 'base-mainnet',
   [holesky.id]: 'holesky',
+};
+
+const alchemyNetworkName: { [key: number]: string } = {
+  [mainnet.id]: 'eth-mainnet',
+  [polygon.id]: 'polygon-mainnet',
+  [arbitrum.id]: 'arb-mainnet',
+  [optimism.id]: 'opt-mainnet',
+  [sepolia.id]: 'eth-sepolia',
+  [base.id]: 'base-mainnet',
+  [holesky.id]: 'eth-holesky',
 };
 
 type _chains = readonly [Chain, ...Chain[]];
@@ -80,16 +91,23 @@ export const chainByName = (name?: string): Chain | null => {
 const transports: _transports = chains.reduce(
   (acc: _transports, chain: Chain) => {
     const infuraNetwork = infuraNetworkName[chain.id];
-    if (!infuraNetwork) {
-      return {
-        ...acc,
-        [chain.id]: http(),
-      };
-    }
+    const infuraUrl =
+      infuraNetwork && INFURA_ID
+        ? `https://${infuraNetwork}.infura.io/v3/${INFURA_ID}`
+        : '';
+    const alchemyNetwork = alchemyNetworkName[chain.id];
+    const alchemyUrl =
+      alchemyNetwork && ALCHEMY_ID
+        ? `https://${alchemyNetwork}.g.alchemy.com/v2/${ALCHEMY_ID}`
+        : '';
+
+    const list = [http()];
+    if (infuraUrl) list.push(http(infuraUrl));
+    if (alchemyUrl) list.push(http(alchemyUrl));
 
     return {
       ...acc,
-      [chain.id]: http(`https://${infuraNetwork}.infura.io/v3/${INFURA_ID}`),
+      [chain.id]: fallback(list.reverse()),
     };
   },
   {} as _transports,
