e88716ecdb57add386c0a418fcd39ac7c39c6b14
dan13ram <dan13ram@gmail.com>
2025-05-27 18:25:45 +0530

    move apps to separate dir

--- .eslintrc.js
+++ .eslintrc.js
@@ -50,13 +50,16 @@ module.exports = {
     'react/prop-types': 'off', // disable prop-types
     'react/require-default-props': 'off', // disable require default props
     'react/react-in-jsx-scope': 'off', // react in jsx scope
-    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'react/jsx-filename-extension': [
+      2,
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    ],
     'react/function-component-definition': 'off',
-    "react/no-array-index-key": "off",
-    "react/jsx-props-no-spreading": "off",
-    "no-use-before-define": "off",
-    "import/no-default-export": "off",
-    "import/prefer-named-exports": "off",
+    'react/no-array-index-key': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'no-use-before-define': 'off',
+    'import/no-default-export': 'off',
+    'import/prefer-named-exports': 'off',
 
     // TS
     '@typescript-eslint/no-explicit-any': 'warn', // disable explicit any
@@ -76,9 +79,9 @@ module.exports = {
     'dist/*',
     'tmp/*',
     '.next',
-    'packages/subgraph/*',
     'packages/graphql/src/zeus/*',
-    'packages/subgraph/subgraph.template.yaml',
+    'apps/subgraph/*',
+    'apps/subgraph/subgraph.template.yaml',
   ],
   settings: {
     'import/resolver': {
